64899b047c7536970f8d274ef88ea05f857c6e44
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-07 02:18:40 -0500

    Added hack commands to the project

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 |
-|	1.- Para crear controladores se escribe en la consola la siguiente ruta
+|	-- Para crear controladores se escribe en la consola la siguiente ruta
 |	$ php artisan make:controller <AwesomeControllerName>
 |
-|	2.- Para crear un modelo y una migración al mismo tiempo:
+|	-- Para crear un modelo y una migración al mismo tiempo:
 |	$ php artisan make:model <AwesomeModelName> -m
 |	
-|	3.- Para resetear las migraciones se utiliza:
+|	-- Para resetear las migraciones se utiliza:
 |	$ php artisan migrate:reset (TODAS LAS MIGRACIONES)
+|
+|	-- Ejecutar migraciones
+|	$ php artisan migrate
 |
 |	NOTA (2): El modelo se toca para cuando se hacen las consultas con Eloquent
 |	(relaciones), las relaciones como tal de la DB es en archivo de la migración
