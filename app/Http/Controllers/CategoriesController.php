61565284945b204cfc71c02c9ba7064194f9ace0
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-08 23:56:58 -0500

    Basic code to get response from categories without relationships

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,10 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
-class CategoriesController extends Controller
-{
-    //
-    public function getAll () {
-    	return Category::All();
+class CategoriesController extends Controller {
+    
+    private $response = array(
+			'code' 		=>	null,
+			'data'		=>	array(),
+			'message'	=>	''
+		);
+    private $codeResponse = null;
+    private $Category = Category::class;
+
+    
+    // Get all categories without relationships
+    public function getAllCategories () {
+    	$this->codeResponse = 200;
+    	$this->response['code'] 	= $this->codeResponse;
+		$this->response['data'] 	= $this->Category::All();
+		$this->response['message'] 	= 'Datos obtenido correctamente.';
+
+    	return response()->json($this->response, $this->codeResponse);
     }
 }
