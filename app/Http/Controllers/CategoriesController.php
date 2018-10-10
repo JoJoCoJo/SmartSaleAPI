466abd1e2ae1c41c42f4c6e891786f969cd7d332
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-09 22:32:37 -0500

    Created function to insert data into categories

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Category;
+
 class CategoriesController extends Controller {
     
     private $response = array(
 			'code' 		=>	null,
-			'data'		=>	array(),
 			'message'	=>	''
 		);
     private $codeResponse = null;
     private $Category = Category::class;
 
-    
+    // This indicates to the validator what have to validate.
+    private $rules = [
+    	'name' => 'required|min:5|max:50',
+    	'description' => 'min:10|max:150'
+    ];
+
+    //This indicates to the validator what messages to show when an error occurs.
+    private $messages = [
+    	'name.required' => 'El nombre no puede quedar vacío.',
+    	'name.min' => 'El nombre debe tener al menos 5 carácteres.',
+    	'name.max' => 'El nombre debe tener máximo 50 carácteres.',
+    	'description.min' => 'La descripción debe tener al menos 10 carácteres.',
+    	'description.max' => 'La descripción debe tener al máximo 150 carácteres.'
+    ];
+
     // Get all categories without relationships
     public function getAll () {
     	$this->codeResponse = 200;
@@ -54,4 +69,32 @@ class CategoriesController extends Controller {
 
     	return response()->json($this->response, $this->codeResponse);
     }
+
+	public function create (Request $request) {
+
+		$validateData = Validator::make($request->all(), $this->rules, $this->messages);
+
+		if ($validateData->fails()) {
+			$this->codeResponse			= 422;
+			$this->response['code'] 	= $this->codeResponse;
+			$this->response['errors'] 	= $validateData->errors();
+			$this->response['message'] 	= 'Han ocurrido algunos errores.';
+		}else{
+			$newCategory = new Category();
+			$newCategory->name = $request->input('name');
+			$newCategory->description = $request->input('description');
+
+			if ($newCategory->save()) {
+				$this->codeResponse 		= 201;
+				$this->response['code']		= $this->codeResponse;
+				$this->response['data']		= $newCategory;
+				$this->response['message'] 	= 'Registro creado con éxito.';
+			}else{
+				$this->codeResponse 		= 500;
+				$this->response['message'] 	= 'No se pudo completar el registro, intentelo más tarde.';
+			}
+		}
+
+		return response()->json($this->response, $this->codeResponse);
+	}
 }
