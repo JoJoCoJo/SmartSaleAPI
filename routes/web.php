9a94d17ffed3875df2ae9b93e04e49a251473c3b
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-10 23:37:44 -0500

    Added some comments to the urls

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// main url to the api for the project
 Route::prefix('api/v1')->group(function () {
+
+	//main url and urls to CRUD data on table categories
 	Route::prefix('/categories')->group(function () {
 		Route::get('/', 'CategoriesController@getAll');
 		Route::get('/*', 'CategoriesController@getAllWithAll');
