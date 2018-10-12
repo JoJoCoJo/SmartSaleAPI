6c0a514be90c17346ac7ca59bf681fccc415e56a
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-11 21:42:53 -0500

    Added var primarykey to eloquent recognoize id_category liker id

--- app/Category.php
+++ app/Category.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
-{
+{	
+	protected $primaryKey = 'id_category';
     public function products () {
     	return $this->hasMany(Product::class, 'category_id', 'id_category');
     }
