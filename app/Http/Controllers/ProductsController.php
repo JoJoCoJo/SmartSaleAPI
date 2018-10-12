5c0ac4bb0c8a2b1fe885e0275ee3e0d1c28aa105
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-11 23:21:01 -0500

    Added basic variables for return response, get messages error from validator and rules

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,13 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Product;
 
 class ProductsController extends Controller
 {
-    //
-    public function getAll () {
-    	$product = Product::with('category')->with('sales')->get();
-    	return $product;
-    }
+        private $response = array(
+    			'code' 		=>	null,
+    			'message'	=>	''
+    		);
+        private $codeResponse = null;
+        private $Product = Product::class;
+
+        // This indicates to the validator what have to validate.
+        private $rules = [
+        	'name' => 'required|min:3|max:50',
+        	'price' => 'required|regex:/([0-9]{1,5}\.[0-9]{1,2})/',
+        	'image' => 'max:100',
+        	'category_id' => 'nullable|integer'
+        ];
+
+        //This indicates to the validator what messages to show when an error occurs.
+        private $messages = [
+        	'name.required' => 'El nombre no puede quedar vacío.',
+        	'name.min' => 'El nombre debe tener al menos 3 carácteres.',
+        	'name.max' => 'El nombre debe tener máximo 50 carácteres.',
+        	'price.required' => 'El precio no quedar vacío.',
+        	'price.regex' => 'El precio no tiene el formato correcto. ej. 57.99 ',
+        	'image.max' => 'La url de la imagen debe tener máximo 100 carácteres.',
+        	'category_id.integer' => 'El id debe ser númerico.'
+
+        ];
+
+        private function Validator (Array $params) {
+        	if (!isset($params['dataToValidate'])) {
+        		return 'dataToValidate is requerid.';
+        	}elseif (!isset($params['rules'])) {
+        		return 'rules is requerid.';
+        	}else{
+        		if (isset($params['messages'])) {
+        			return Validator::make($params['dataToValidate'], $params['rules'], $params['messages']);
+        		}else{
+        			return Validator::make($params['dataToValidate'], $params['rules']);
+        		}
+        	}
+        }
 }
