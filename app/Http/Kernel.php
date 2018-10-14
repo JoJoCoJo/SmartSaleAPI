c6d1e92b963943d10bd859925b946647bf937911
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-13 22:15:19 -0500

    Removed unusefull files

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,6 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'returnJSON' => \App\Http\Middleware\ReturnJSON::class,
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
