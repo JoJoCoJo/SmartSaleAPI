de1b0e3a0d226ae0282692999b9afa14050bd799
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-25 21:44:27 -0500

    Created model and migration to forecast table

---
+++ database/migrations/2018_10_26_015319_create_forecasts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForecastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forecasts', function (Blueprint $table) {
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->increments('id_forecast');
+            $table->tinyInteger('type_forecast');
+            $table->longText('forecastData');
+            $table->unsignedInteger('sale_id');
+            $table->foreign('sale_id')->references('id_sale')->on('sales');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forecasts');
+    }
+}
