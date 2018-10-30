c18dcd244f57797e9a0a860422c8504b20732faa
JoJoCJ92 <jojocojo@hotmail.com>
2018-10-29 21:21:48 -0600

    Basic structure to Forecast controller

--- app/Http/Controllers/ForecastController.php
+++ app/Http/Controllers/ForecastController.php
@@ -3,8 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Forecast;
 
-class ForecastController extends Controller
-{
-    //
+class ForecastController extends Controller {
+	private $response = [
+		'code' 		=>	null,
+		'message'	=>	''
+	];
+
+	private $codeResponse = null;
+
+	private $Forecast = Forecast::class;
+
+	// This indicates to the validator what have to validate.
+	private $rules = [
+		/*'date_sale' => 'required|date',
+		'total_units_sales' => 'required|integer',
+		'type_sale' => 'required|integer',
+		'user_id' => 'required|integer',
+		'category_id' => 'nullable|integer',*/
+	];
+
+	//This indicates to the validator what messages to show when an error occurs.
+	private $messages = [
+		/*'date_sale.required' => 'La fecha no puede quedar vacío.',
+		'date_sale.date' => 'El formato de la fecha no válido.',
+		'total_units_sales.required' => 'El total de productos vendidos no puede quedar vació.',
+		'total_units_sales.integer' => 'El total de productos vendidos debe ser númerico.',
+		'type_sale.required' => 'Debe seleccionar un tipo de venta.',
+		'type_sale.integer' => 'El tipo de venta debe ser númerico.',
+		'user_id.required' => 'El usuario es requerido.',
+		'user_id.integer' => 'El id del usuario debe ser númerico.',
+		'category_id.integer' => 'El id de la categoria debe ser númerico.',*/
+	];
+
+	private function Validator (Array $params) {
+		if (!isset($params['dataToValidate'])) {
+			return 'dataToValidate is requerid.';
+		}elseif (!isset($params['rules'])) {
+			return 'rules is requerid.';
+		}else{
+			if (isset($params['messages'])) {
+				return Validator::make($params['dataToValidate'], $params['rules'], $params['messages']);
+			}else{
+				return Validator::make($params['dataToValidate'], $params['rules']);
+			}
+		}
+	}
 }
